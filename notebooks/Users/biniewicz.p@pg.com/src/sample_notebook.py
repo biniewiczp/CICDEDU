059c8ec3304f2cef2224780023e7800fb72ba3b7
biniewiczp <65956254+biniewiczp@users.noreply.github.com>
2020-05-26 21:16:29 +0200

    Update: Data notebooks

---
+++ notebooks/Users/biniewicz.p@pg.com/src/sample_notebook.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+# MAGIC %run ./InstallDepedencies
+
+# COMMAND ----------
+
+import numpy
+print ( "***** Hello World ********")
+numpy.test ('full')
+print ("***** Hello World********")
