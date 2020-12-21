243447f32f4c7b346b72f4f9726bd2e9ef471bdc
biniewiczp <biniewicz.p@pg.com>
2020-12-21 17:17:16 +0100

    this 3 rd commit

--- notebooks/Users/biniewicz.p@pg.com/src/sample_notebook.py
+++ notebooks/Users/biniewicz.p@pg.com/src/sample_notebook.py
@@ -17,3 +17,22 @@ count = 0
 lst=list()
 hours_full=list()
 hour=dict()
+if len(fname) < 1 :
+    fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+lst=list()
+hours_full=list()
+hour=dict()
+for line in fh:
+    #print(line.rstrip())
+    if not line.startswith('From'): continue
+    lst=line.rstrip().split()
+    if len(lst)>2:
+         hours_full=lst[5].rstrip().split(':')
+         hour[hours_full[0]]=hour.get(hours_full[0],0)+1
+sorted(hour.items())
+
+for i,j in sorted(hour.items()):
+    print(i,j)
