51189d3c4dbc770a160df528339768aa4c2ba1b4
biniewiczp <biniewicz.p@pg.com>
2020-12-21 17:01:07 +0100

    This is my first commit

--- notebooks/Users/biniewicz.p@pg.com/src/sample_notebook.py
+++ notebooks/Users/biniewicz.p@pg.com/src/sample_notebook.py
@@ -9,3 +9,11 @@ numpy.test ('full')
 print ("***** DUPA ***** ")
 print ("proba 2")
 print ("***** Hello World********")
+if len(fname) < 1 :
+    fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+lst=list()
+hours_full=list()
+hour=dict()
